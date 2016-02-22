c63277cff365c09e416ded5abbcb6e1cb2f7a70c
Alex <alexbruceharley@gmail.com>
2016-02-22 18:27:04 +1300

    sql scripts

---
+++ SqlScripts/Stores.sql
@@ -0,0 +1,17 @@
+USE [ShellDb]
+GO
+
+/****** Object:  Table [dbo].[Stores]    Script Date: 22/02/2016 6:26:14 PM ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE TABLE [dbo].[Stores](
+	[Id] [int] NOT NULL,
+	[Name] [nvarchar](50) NOT NULL
+) ON [PRIMARY]
+
+GO
+
---
+++ SqlScripts/Users.sql
@@ -0,0 +1,27 @@
+USE [ShellDb]
+GO
+
+/****** Object:  Table [dbo].[Users]    Script Date: 22/02/2016 6:26:25 PM ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE TABLE [dbo].[Users](
+	[Id] [uniqueidentifier] NOT NULL,
+	[UserName] [nvarchar](max) NOT NULL,
+	[PasswordHash] [nvarchar](max) NULL,
+	[SecurityStamp] [nvarchar](max) NULL,
+	[StoreUserId] [int] NULL,
+	[EmailAddress] [nvarchar](50) NULL,
+	[FirstName] [nvarchar](50) NULL,
+	[LastName] [nvarchar](50) NULL,
+ CONSTRAINT [PK_Users] PRIMARY KEY CLUSTERED 
+(
+	[Id] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+) ON [PRIMARY] TEXTIMAGE_ON [PRIMARY]
+
+GO
+
